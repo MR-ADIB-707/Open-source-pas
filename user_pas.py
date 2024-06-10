561ecfc98c77ecf4a4c953deb95ff724ab865480
MR-ADIB-707 <162477030+MR-ADIB-707@users.noreply.github.com>
2024-06-10 10:18:51 +0600

    Assalamoalaikom
    SCRIPT MAKE TUTORIAL ON MY PAGE 
    PAGE NAME : HELLO WORLD
    PAGE LINK    : https://www.facebook.com/ahmedadib033

---
+++ user_pas.py
@@ -0,0 +1,11 @@
+
+def psx():
+	#print(adib)
+	user=input("ENTER THE TOOLS USERNAME : ")
+	pss=input("ENTER THE TOOLS PASSWORD : ")
+	#USER& PASSWORD ANY NAME YOUR CHOICE 
+	if user in ['noob','NOOB'] and pss in ['boss','BOSS']:
+		#zoker()
+	else:
+		print(">> YOUR USER & PASSWORD INCURRECT ")
+		exit() 
